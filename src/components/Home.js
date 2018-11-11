d0e49eb7987b91278e20c83e8af39e3d93c3a57b
Jamie Davis <jamiedavis@unknown-f0:18:98:31:68:10.lan>
2018-11-11 19:29:02 +0000

    final commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,20 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
+import PokeBall from "../Pokeball.png";
+import { connect } from "react-redux";
 
 class Home extends Component {
-  state = {
-    posts: []
-  };
-
-  componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-      console.log(res);
-      this.setState({ posts: res.data.slice(0, 10) });
-    });
-  }
   render() {
-    const { posts } = this.state;
+    console.log(this.props);
+    const { posts } = this.props;
     const postList = posts.length ? (
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={PokeBall} alt="a poke ball" />
             <div className="card-content">
               <Link to={"/" + post.id}>
-                <span className="card-title">{post.title}</span>
+                <span className="card-title red-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>
             </div>
@@ -41,4 +34,10 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+
+export default connect(mapStateToProps)(Home);
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,31 +1,52 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { connect } from "react-redux";
+import { deletePost } from "../sctions/postActions";
 
 class Post extends Component {
-  state = {
-    post: null
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push("/");
   };
-  componentDidMount() {
-    let id = this.props.match.params.post_id;
-    axios.get("https://jsonplaceholder.typicode.com/posts/" + id).then(res => {
-      this.setState({
-        post: res.data
-      });
-      console.log(res);
-    });
-    this.setState({ id });
-  }
+
   render() {
-    const post = this.state.post ? (
+    console.log(this.props);
+    const post = this.props.post ? (
       <div className="post">
-        <h4 className="center">{this.state.post.title}</h4>
-        <p>{this.state.post.body}</p>
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey">Delete Post</button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading Post...</div>
     );
-    return <div className="container">{post}</div>;
+    return (
+      <div className="container" onClick={this.handleClick}>
+        {post}
+      </div>
+    );
   }
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  return {
+    post: state.posts.find(post => {
+      return post.id === id;
+    })
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    deletePost: id => {
+      dispatch(deletePost(id));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Post);
